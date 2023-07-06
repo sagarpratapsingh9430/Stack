e9e1ae32c3b5153f7d106b2ea218fb0ec93e3a95
Sagar Pratap Singh <sagarpratapsingh9432@gmail.com>
2023-07-06 19:17:16 +0530

    Ability to create a stack using push method

--- src/com/bridgelabz/stack/Main.java
+++ src/com/bridgelabz/stack/Main.java
@@ -3,6 +3,10 @@ package com.bridgelabz.stack;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to stack program: ");
-       
+        Stack stack = new Stack();
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
+        stack.print();
     }
 }
--- src/com/bridgelabz/stack/Stack.java
+++ src/com/bridgelabz/stack/Stack.java
@@ -6,5 +6,24 @@ public class Stack {
     public boolean isEmpty() {
         return head == null;
     }
+    void push(int data){
+        Node node = new Node(data);
 
+        if (isEmpty()){
+            head = node;
+            return;
+        }
+        node.next = head;
+        head = node;
+    }
+
+
+    void print(){
+        Node temp = head;
+        while (temp != null){
+            System.out.print(temp.data + "-->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
 }
