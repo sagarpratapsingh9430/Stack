140dbb46ef74d55d0696dc67633d0be520806879
Sagar Pratap Singh <sagarpratapsingh9432@gmail.com>
2023-07-06 19:13:52 +0530

    Welcome to stack program

---
+++ src/com/bridgelabz/stack/Node.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.stack;
+
+public class Node {
+    int data;
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
---
+++ src/com/bridgelabz/stack/Stack.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.stack;
+
+public class Stack {
+    Node head;
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+}
